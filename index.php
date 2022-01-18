db329b4d751d7d11fcd9f8291c53e062715b2031
Simon-VVV <simon.vierbuecher@gmail.com>
2022-01-18 22:52:27 +0100

    php test und verkackte footer versuche

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <html lang="de">
+    <meta charset="utf-8" />
+    <title>kyaoq.me</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+</head>
+
+<container id="container">
+
+    <header id="header">
+
+        <img src="favicon.ico" style="width: 50px;">
+
+        kyaoq.me
+    </header>
+
+    <body id="main">
+
+        <div id="untertitel">
+            Why are you torturing me with Comic Sans, Nancy?
+        </div>
+
+        <?php echo "php test" ?>
+
+        <div id="pictures">
+
+            <picture class="pic">
+
+                <img src="img/akape.png" style="width: auto;">
+
+                <a href="img/akape.png" download="akape.png">
+                    <button class="download">
+                        <i class="fa fa-download"></i>
+                        Download
+                    </button>
+                </a>
+
+            </picture>
+
+        </div>
+
+    </body>
+
+    <footer id="footer">
+
+        <div>
+            This is my first try at making a website. Maybe I will make a Blog out of this that nobody will ever use.
+            Most likely I will lose interest after tinkering with it for a little bit. Who knows. Let's see.
+            How the fuck did you find this website anyways? Join this Discord and tell me: https://discord.gg/4Mgx4TpXP2
+        </div>
+
+    </footer>
+
+</container>
+
+</html>
